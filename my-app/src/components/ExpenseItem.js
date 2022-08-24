a9cd4fbf71e0eb977b8d8e32f2540970dac2fcbc
BiswarupJana <biswaruprx21@gmail.com>
2022-08-24 18:59:31 +0530

    initial product listed

--- my-app/src/components/ExpenseItem.js
+++ my-app/src/components/ExpenseItem.js
@@ -1,5 +1,17 @@
-function ExpenseItem(){
-    return <h2>Expense item</h2>
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+
+    return (
+        <div className="expense-item">
+            <ExpenseDate date={props.date}/>
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+            </div>
+            <div className="expense-item__price">${props.amount}</div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
