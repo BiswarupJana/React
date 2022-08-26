c0ccc3604ce5ba8192c55014b27f008df3daca4e
BiswarupJana <biswaruprx21@gmail.com>
2022-08-26 12:13:43 +0530

    add year filter

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
+
 const App=()=>{
 
   const expenses = [
@@ -29,7 +30,7 @@ const App=()=>{
   const addExpenseHandler=(expense)=>{
     console.log('In App.js');
     console.log(expense);
-  };
+   };
 
   // return React.createElement('div',{},
   // React.createElement('h2',{},"Let's get started!"),
