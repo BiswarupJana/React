1f1d5707bb66e6e57c56f1dee216e790b6bc52bb
BiswarupJana <biswaruprx21@gmail.com>
2022-08-26 12:43:41 +0530

    removed unnessary codes

--- my-app/src/components/Expenses/ExpenseItem.js
+++ my-app/src/components/Expenses/ExpenseItem.js
@@ -1,25 +1,25 @@
-import React, { useState } from 'react';
+import React /*{ useState }*/ from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-    const [title, setTitle] = useState(props.title);
+    // const [title, setTitle] = useState(props.title);
     // console.log('ExpenseItem evaluated by React');
     // setTimeout(4);
 
-    const clickHandler = () => {
-        setTitle('Update!');
-        console.log(title);
-    };
+    // const clickHandler = () => {
+    //     setTitle('Update!');
+    //     console.log(title);
+    // };
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
             </div>
             <div className="expense-item__price">${props.amount}</div>
-            <button onClick={clickHandler}>change Items</button>
+            {/* <button onClick={clickHandler}>change Items</button> */}
         </Card>
     );
 }
