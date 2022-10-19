f0e11f4acdecf83bea030379f3bbd7289854c4c0
BiswarupJana <biswaruprx21@gmail.com>
2022-10-19 11:36:43 +0530

    hooks examples

---
+++ react hooks/useRef()/my-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import UseRef from './components/UseRef';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <UseRef/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
