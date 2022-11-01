b81efc87b157cabe4d75b7e19ba415eb985d6899
BiswarupJana <biswaruprx21@gmail.com>
2022-11-01 22:23:17 +0530

    cart modified

--- Food_order/src/App.js
+++ Food_order/src/App.js
@@ -2,6 +2,7 @@ import React,{useState} from 'react';
 import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import CartProvider from './store/CartProvider';
 
 function App() {
 
@@ -17,13 +18,13 @@ function App() {
 
 
   return (
-    <React.Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onHideCart={hideCartHandler}/>}
       <Header onShowCart={showCartHandler} />
       <main>
       <Meals />
       </main>
-    </React.Fragment>
+    </CartProvider>
   );
 }
 
--- Food_order/src/store/CartProvider.js
+++ Food_order/src/store/CartProvider.js
@@ -1,3 +1,48 @@
-const CartProvider=props=>{};
+import { useReducer } from 'react';
+
+import CartContext from './cart-context';
+
+const defaultCartState={
+    items: [], 
+    totalAmount:0
+}
+
+const cartReducer=(state, action)=>{
+    if(action.type=== 'ADD'){
+        const updatedItems=state.items.concat(action.item);
+        const updatedTotalAmount=state.totalAmount + action.item.price * action.item.amount;
+        return{
+            items: updatedItems,
+            totalAmount: updatedTotalAmount,
+        };
+    }
+    return defaultCartState;
+};
+
+const CartProvider=props=>{
+
+    const [cartState, dispatchartAction]=useReducer(cartReducer, defaultCartState);
+
+    const addItemFromCartHandler=(item)=>{
+        dispatchartAction({type: 'ADD', item: item});
+    };
+
+    const removeItemFromCartHandler=(id)=>{
+        dispatchartAction({type: 'REMOVE', id: id});
+    };
+    
+    const cartContext={
+        items:cartState.items,
+        totalAmount:cartState.totalAmount,
+        addItem: addItemFromCartHandler,
+        removeItem:removeItemFromCartHandler,
+    };
+
+    return (
+    <CartContext.Provider value={cartContext}>
+        {props.children}
+    </CartContext.Provider>
+    );
+};
 
 export default CartProvider;
