b28c988c0708983d80647f857bd950cb2966efb8
BiswarupJana <biswaruprx21@gmail.com>
2022-11-27 20:08:42 +0530

    Done

--- sending_https/project 1/src/App.js
+++ sending_https/project 1/src/App.js
@@ -1,24 +1,99 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useCallback } from 'react';
+
+import MoviesList from './components/MoviesList';
 import './App.css';
+import AddMovie from './components/AddMovie';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError]=useState(null);
+
+ 
+  
+  const fetchMoviesHandler =useCallback(async ()=> {
+    setIsLoading(true);
+    setError(null);
+
+    try{
+      const response = await fetch('https://react-http-a7fdf-default-rtdb.firebaseio.com/movies.json');
+      if(!response.ok){
+        throw new Error('Something Went wrong!');
+      }
+
+      const data = await response.json();
+
+      const loadedMovies =[];
+
+      for(const key in data){
+        loadedMovies.push({
+          id:key, 
+          title:data[key].title,
+          openingText:data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+  
+      // const transformedMovies = data.map(movieData => {
+      //   return {
+      //     id: movieData.episode_id,
+      //     title: movieData.title,
+      //     openingText: movieData.opening_crawl,
+      //     releaseDate: movieData.release_date,
+      //     url: movieData.url
+      //   };
+      // });
+      setMovies(loadedMovies);
+    } catch(error){
+      setError(error.message);
+    }
+    setIsLoading(false);
+    
+  }, []);
+
+  useEffect(()=>{
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  async function addMovieHandler(movie){
+    const response =await fetch('https://react-http-a7fdf-default-rtdb.firebaseio.com/movies.json',{
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data=await response.json();
+    console.log(data);
+  }
+
+  
+
+  let content =<p>Found no Movies</p>
+  if(movies.length>0){
+    content=<MoviesList movies={movies} />;
+  }
+  
+  if(error){
+    content= <p>{error}</p>
+  }
+  if(isLoading){
+    content=<p>Loading...</p>;
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler}/>
+      </section>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {content}
+      </section>
+    </React.Fragment>
   );
 }
 
