c83e6a769164fcd40f08b9c725331e584d753461
BiswarupJana <biswaruprx21@gmail.com>
2022-12-21 11:57:13 +0530

    https custom hook done

--- sending_https/project 1/src/App.js
+++ sending_https/project 1/src/App.js
@@ -16,7 +16,7 @@ function App() {
     setError(null);
 
     try{
-      const response = await fetch('https://react-http-a7fdf-default-rtdb.firebaseio.com/movies.json');
+      const response = await fetch('https://react-https-b5b53-default-rtdb.firebaseio.com/movies.json');
       if(!response.ok){
         throw new Error('Something Went wrong!');
       }
@@ -56,7 +56,7 @@ function App() {
   }, [fetchMoviesHandler]);
 
   async function addMovieHandler(movie){
-    const response =await fetch('https://react-http-a7fdf-default-rtdb.firebaseio.com/movies.json',{
+    const response =await fetch('https://react-https-b5b53-default-rtdb.firebaseio.com/movies.json',{
       method: 'POST',
       body: JSON.stringify(movie),
       headers: {
