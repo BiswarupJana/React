a3ab96a1b9518a536dd4e5619aae940336d3734c
BiswarupJana <biswaruprx21@gmail.com>
2023-01-14 18:34:11 +0530

    add error handler
    Co-authored-by: Tridib Ghosh <tridib1002@users.noreply.github.com>

--- Food_order/src/components/Meals/AvailableMeals.js
+++ Food_order/src/components/Meals/AvailableMeals.js
@@ -6,11 +6,19 @@ import classes from './AvailableMeals.module.css';
 
 const AvailableMeals = () => {
     const [meals, setMeals] = useState([]);
-    const [isLoading, setIsLoading]=useState(true);
+    const [isLoading, setIsLoading] = useState(true);
+    const [httpError, setHttpError] = useState(null);
 
     useEffect(() => {
         const fetchMeals = async () => {
-            const response = await fetch('https://react-https-b5b53-default-rtdb.firebaseio.com/meals.json');
+            const response = await fetch(
+                'https://react-https-b5b53-default-rtdb.firebaseio.com/meals.json'
+            );
+
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
+
             const responseData = await response.json();
 
             const loadedMeals = [];
@@ -26,17 +34,32 @@ const AvailableMeals = () => {
             setMeals(loadedMeals);
             setIsLoading(false);
         };
-        fetchMeals();
+
+
+        fetchMeals().catch((error) => {
+            setIsLoading(false);
+            setHttpError(error.message);
+        });
     }, []);
 
-    if(isLoading){
-        return <section className={classes.MealsLoading}>
+
+    if (isLoading) {
+        return (
+        <section className={classes.MealsLoading}>
             <p>Loading....</p>
         </section>
+        );
     }
 
-    const mealsList = meals.map((meal) => (
-        <MealItem
+    if (httpError) {
+        return (
+            <section className={classes.MealsError}>
+                <p>{httpError}</p>
+            </section>
+        );
+    } else {
+    const mealsList = meals?.map((meal) => {
+        return <MealItem
             key={meal.id}
             id={meal.id}
             name={meal.name}
@@ -44,14 +67,16 @@ const AvailableMeals = () => {
             price={meal.price}
 
         />
-    ));
+    });
 
     return <section className={classes.meals}>
         <Card>
             {mealsList}
         </Card>
     </section>
-};
+
+}};
+
 
 
 export default AvailableMeals;
