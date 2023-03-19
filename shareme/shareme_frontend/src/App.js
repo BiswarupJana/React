3ea58e02a2d7cfa8caab66e7437d002baac44712
Biswarup Jana <biswaruprx21@gmail.com>
2023-03-19 17:45:48 +0530

    google api added and user profileObj getting

--- shareme/shareme_frontend/src/App.js
+++ shareme/shareme_frontend/src/App.js
@@ -1,15 +1,39 @@
-import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Home from './container/Home';
+import { gapi } from "gapi-script";
 
+const clientId = process.env.REACT_APP_GOOGLE_API_TOKEN;
 const App = () => {
-  return (
-    <Routes>
-      <Route path="login" element={<Login />}/>
-      <Route path="/*" element={<Home />}/>
-    </Routes>
-  )
-}
+  
+    useEffect(() => {
+      function start() {
+        gapi.client.init({
+          clientId: clientId,
+          scope: ""
+        })
+      };
+      gapi.load('client:auth2', start)
+    },[]);
 
-export default App
+
+    // gapi.load("client:auth2", () => {
+    //   gapi.client.init({
+    //     client_id:
+    //       '956540138964-9rruepdroolfvap1rr5vpbj3bigpf68l.apps.googleusercontent.com',
+    //     plugin_name: "undefined",
+    //     projectId:process.env.REACT_APP_SANITY_PROJECT_ID,
+    //   });
+    // });
+
+
+    return (
+      <Routes>
+        <Route path="login" element={<Login />} />
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    )
+  }
+
+  export default App;
--- shareme/shareme_frontend/src/components/Login.jsx
+++ shareme/shareme_frontend/src/components/Login.jsx
@@ -1,52 +1,67 @@
 import React from 'react';
 import GoogleLogin from 'react-google-login';
 import { useNavigate } from 'react-router-dom';
-import {FcGoogle} from 'react-icons/fc';
+import { FcGoogle } from 'react-icons/fc';
 import shareVideo from '../assets/share.mp4';
 import logo from '../assets/logowhite.png';
-import { useGoogleLogin } from 'react-google-login';
+import { client } from '../client';
 
 const Login = () => {
-  const responseGoogle =(response)=>{
-    console.log(response);
+  const navigate =useNavigate();
+  
+  const responseGoogle = (response) => {
+    localStorage.setItem('user', JSON.stringify(response.profileObj));
+    const {name, googleId, imageUrl }=response.profileObj;
+    const doc ={
+      _id: googleId,
+      _type: 'user',
+      userName: name,
+      image: imageUrl,
+    }
+
+    client.createIfNotExists(doc)
+    .then(()=>{
+      navigate('/',{replace: true})
+    })
+    console.log(response.profileObj);
   };
   return (
     <div className="flex justify-center items-center flex-col h-screen">
       <div className="relative w-full h-full">
-        <video 
-        src={shareVideo}
-        type="video/mp4"
-        loop
-        controls={false}
-        muted
-        autoPlay
-        className="w-full h-full object-cover"
+        <video
+          src={shareVideo}
+          type="video/mp4"
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className="w-full h-full object-cover"
         />
 
-      <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 buttom-0 bg-blackOverlay h-full">
-          <img src={logo} width="130px" alt="logo"/>
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 buttom-0 bg-blackOverlay h-full">
+          <img src={logo} width="130px" alt="logo" />
           <div className="shadow-2xl">
-            <GoogleLogin 
+            <GoogleLogin
               clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
-              render={(renderProps)=>(
+              render={(renderProps) => (
                 <button
-                type="button"
-                className="bg-mainColor flex justify-center items-center m-3 p-3 rounded-lg cursor-pointer outline-none"
-                onClick={renderProps.onClick}
-                disabled={renderProps.disabled}
+                  type="button"
+                  className="bg-mainColor flex justify-center items-center m-3 p-3 rounded-lg cursor-pointer outline-none"
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
                 >
-                  <FcGoogle className="mr-4"/>Sign in with Google
+                  <FcGoogle className="mr-4" />Sign in with Google
                 </button>
               )}
               onSuccess={responseGoogle}
               onFailure={responseGoogle}
-              cookiePolicy="single_host_origin"
+              cookiePolicy={'single_host_origin'}
             />
           </div>
-      </div>
+        </div>
       </div>
     </div>
   )
 }
 
-export default Login
+export default Login;
