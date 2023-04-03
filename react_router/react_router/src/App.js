87811f9e758cf06f485c528130aada641f848777
Biswarup Jana <biswaruprx21@gmail.com>
2023-04-03 20:56:57 +0530

    react_route

---
+++ react_router/react_router/src/App.js
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+import HomePage from "./pages/Home";
+
+createBrowserRouter({
+  {path: '/', element: <Home /> },
+  {}
+})
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
