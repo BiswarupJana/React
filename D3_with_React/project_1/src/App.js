869beb0c227fefdf190e07b5a79bd1c7fe77c487
Biswarup Jana <biswaruprx21@gmail.com>
2023-06-25 13:31:04 +0530

    D3 circle, line , curveCardinal added

--- D3_with_React/project_1/src/App.js
+++ D3_with_React/project_1/src/App.js
@@ -1,32 +1,50 @@
 import React ,{useRef, useEffect, useState} from 'react';
 import './App.css';
-import {select} from "d3";
-
+import { select, line, curveCardinal, axisBottom } from "d3";
 
 function App() {
-  const [data, setData] = useState([25, 30 ,45, 60 , 20])
-  const svgRef=useRef();
+  const [data, setData] = useState([25, 30, 45, 60, 20, 65, 75]);
+  const svgRef = useRef();
   useEffect(() => {
-    // console.log(svgRef);  
-    const svg= select(svgRef.current);  
+    // console.log(svgRef);
+    const svg = select(svgRef.current);
+    const xAxis = axisBottom(svg);
+    const myLine = line()
+      .x((value, index) => index * 50)
+      .y((value) => 150 - value)
+      .curve(curveCardinal);
+
+    /////circle //
+    // svg
+    //   .selectAll("circle")
+    //   .data(data)
+    //   .join("circle")
+    //   .attr("r", (value) => value)
+    //   .attr("cx", (value) => value * 2)
+    //   .attr("cy", (value) => value * 2)
+    //   .attr("stroke", "red");
+
+    ////line////
     svg
-    .selectAll("circle")
-    .data(data)
-    .join(
-      enter => enter.append("circle").attr("class", "new").style('background', 'red'),
-      update => update.attr("class", "update").style('background', 'blue'),
-      exit => exit.remove()
-    );
-  }, [data])
-  
+      .selectAll("path")
+      .data([data])
+      .join("path")
+      .attr("d", (value) => myLine(value))
+      .attr("fill", "none")
+      .attr("stroke", "blue");
+  }, [data]);
+
   return (
     <React.Fragment>
       <svg ref={svgRef}></svg>
       <br />
-      <button onClick={()=>setData(data.map(value=> value + 5))}>
+      <button onClick={() => setData(data.map((value) => value + 5))}>
         Update data
       </button>
+      <button onClick={() => setData(data.filter((value) => value <= 35))}>
+        Filter Data
+      </button>
     </React.Fragment>
-  )
+  );
 }
 export default App;
