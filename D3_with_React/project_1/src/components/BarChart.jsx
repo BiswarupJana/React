9e51ec0435843e9e07cf970f08699a2a6d14cd76
Biswarup Jana <biswaruprx21@gmail.com>
2023-06-28 13:21:37 +0530

    trandsition added to bar head text

--- D3_with_React/project_1/src/components/BarChart.jsx
+++ D3_with_React/project_1/src/components/BarChart.jsx
@@ -5,30 +5,30 @@ import classes from "./BarChart.module.css";
 const BarChart = () => {
   const [data, setData] = useState([25, 30, 45, 60, 20, 65, 75]);
   const svgRef = useRef();
+  // will be called initially and on every data change
   useEffect(() => {
-    // console.log(svgRef);
     const svg = select(svgRef.current);
+
+    // scales
     const xScale = scaleBand()
       .domain(data.map((value, index) => index))
       .range([0, 300])
       .padding(0.5);
 
     const yScale = scaleLinear().domain([0, 150]).range([150, 0]);
+
     const colorScale = scaleLinear()
-      .domain([0, 75, 150])
-      .range(["green", "blue", "red"]);
+      .domain([75, 100, 150])
+      .range(["green", "orange", "red"])
+      .clamp(true);
 
+    // create x-axis
     const xAxis = axisBottom(xScale).ticks(data.length);
-    svg
-      .selectAll(".x-axis")
-      .style("transform", "translateY(150px)")
-      .call(xAxis);
+    svg.select(".x-axis").style("transform", "translateY(150px)").call(xAxis);
 
+    // create y-axis
     const yAxis = axisRight(yScale);
-    svg
-      .selectAll(".y-axis")
-      .style("transform", "translateX(300px)")
-      .call(yAxis);
+    svg.select(".y-axis").style("transform", "translateX(300px)").call(yAxis);
 
     svg
       .selectAll(".bar")
@@ -39,14 +39,21 @@ const BarChart = () => {
       .attr("x", (value, index) => xScale(index))
       .attr("y", -150)
       .attr("width", xScale.bandwidth())
-      .on("mouseenter", (value, index) => {
+      .on("mouseenter", (event, value) => {
+        const index = svg.selectAll(".bar").nodes().indexOf(event.target);
         svg
           .selectAll(".tooltip")
           .data([value])
-          .join("text")
+          .join((enter) => enter.append("text").attr("y", yScale(value) - 4))
+          .text([value])
           .attr("class", "tooltip")
-          .text(value);
+          .attr("x", xScale(index) + xScale.bandwidth() / 2)
+          .attr("text-anchor", "middle")
+          .transition()
+          .attr("y", yScale(value) - 8)
+          .attr("opacity", 1);
       })
+      .on("mouseleave", () => svg.select(".tooltip").remove())
       .transition()
       .attr("fill", colorScale)
       .attr("height", (value) => 150 - yScale(value));
